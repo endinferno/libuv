ba13d7d694c40f9b7ebe1dd3756b15155c6ab373
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-05-23 10:14:57 +0800

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-pushd /home/endinferno/project/libuv/build
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+BUILD_DIR=${PROJECT_ROOT}/build
+INSTALL_DIR=${PROJECT_ROOT}/install
+pushd ${BUILD_DIR}
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-    -DCMAKE_INSTALL_PREFIX=/home/endinferno/project/libuv/install \
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
     -DLIBUV_BUILD_TESTS=ON \
     -DLIBUV_BUILD_BENCH=ON \
     ..
